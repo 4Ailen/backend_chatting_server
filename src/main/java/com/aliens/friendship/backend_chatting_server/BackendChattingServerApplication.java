9ebe0eb584b03a43a6c3398b8d59a2df9735cc5d
김명준 <86913355+mjj111@users.noreply.github.com>
2023-05-19 13:56:10 +0900

    프로젝트 초기화

---
+++ src/main/java/com/aliens/friendship/backend_chatting_server/BackendChattingServerApplication.java
@@ -0,0 +1,13 @@
+package com.aliens.friendship.backend_chatting_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChattingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChattingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliens/friendship/backend_chatting_server/BackendChattingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliens.friendship.backend_chatting_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChattingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
