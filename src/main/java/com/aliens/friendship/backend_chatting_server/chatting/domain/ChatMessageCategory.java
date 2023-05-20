c98c0f92ae0858ffdbc1ced2708ace89cc2211f8
김명준 <86913355+mjj111@users.noreply.github.com>
2023-05-20 21:30:28 +0900

    Chore #5 chat 도메인의 Enum vs게임 메시지와 공지 메시지, 보통 메시지를 구분하기위한 enum 구현

---
+++ src/main/java/com/aliens/friendship/backend_chatting_server/chatting/domain/ChatMessageCategory.java
@@ -0,0 +1,7 @@
+package com.aliens.friendship.backend_chatting_server.chatting.domain;
+
+public enum ChatMessageCategory {
+    VS_GAME_MESSAGE,
+    NORMAL_MESSAGE,
+    ALL_NOTICE_MESSAGE // 채팅이 곧 끝난다는 마감 공지, 처벌 주의 공지 메시지 용도
+}
