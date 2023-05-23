22eeaff4693c865e043e2f7fc7bc9881703a3cc9
김명준 <86913355+mjj111@users.noreply.github.com>
2023-05-23 09:01:53 +0900

    Design #7 Dto변환 메서드 추가 /* RequestDto -> ResponseDto*/

--- src/main/java/com/aliens/friendship/backend_chatting_server/chatting/dto/ChatResponseDto.java
+++ src/main/java/com/aliens/friendship/backend_chatting_server/chatting/dto/ChatResponseDto.java
@@ -5,13 +5,13 @@ import com.aliens.friendship.backend_chatting_server.chatting.domain.ChatMessage
 import lombok.*;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ChatResponseDto {
-
     private Long roomId;
     private Long senderId;
     private Long receiverId;
@@ -19,7 +19,7 @@ public class ChatResponseDto {
     private ChatMessageCategory messageCategory;
 
     private Long chatId;
-    private Instant createTime;
+    private String createTime;
     private boolean read;
 
     /* Entity -> Dto*/
@@ -30,7 +30,18 @@ public class ChatResponseDto {
         this.message = chat.getMessage();
         this.messageCategory = chat.getMessageCategory();
         this.chatId = chat.getChatId();
-        this.createTime = chat.getCreateTime();
+        this.createTime = chat.getCreateTime().toString();
         this.read = chat.isRead();
+    }
+
+    /* RequestDto -> ResponseDto*/
+    public ChatResponseDto(ChatRequestDto requestDto) {
+        this.roomId = requestDto.getRoomId();
+        this.senderId = requestDto.getSenderId();
+        this.receiverId = requestDto.getReceiverId();
+        this.message = requestDto.getMessage();
+        this.messageCategory = requestDto.getMessageCategory();
+        this.createTime = LocalDateTime.now().toString();
+        this.read = false;
     }
 }
