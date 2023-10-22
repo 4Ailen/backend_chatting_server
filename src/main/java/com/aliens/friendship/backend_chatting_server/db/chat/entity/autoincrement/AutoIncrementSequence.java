4af5f2b9ef422452da3aaba4fc71b16dffa073e7
coke98 <jhs9832@gmail.com>
2023-10-22 20:30:53 +0900

    fix: ChatEntity AutoIncrement Id 적용
    - 아이디 값 관리를 위해 AutoIncrement 관련 코드 재적용

---
+++ src/main/java/com/aliens/friendship/backend_chatting_server/db/chat/entity/autoincrement/AutoIncrementSequence.java
@@ -0,0 +1,16 @@
+package com.aliens.friendship.backend_chatting_server.db.chat.entity.autoincrement;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "auto_sequence")
+public class AutoIncrementSequence {
+
+    @Id
+    private String id;
+    private Long seq;
+}
