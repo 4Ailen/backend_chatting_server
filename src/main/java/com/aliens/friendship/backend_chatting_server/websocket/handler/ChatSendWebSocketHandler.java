db5dc9e38c61d7906cae4bb7b23262f93e45e364
coke98 <jhs9832@gmail.com>
2023-10-22 20:28:43 +0900

    fix: 핸들러 메서드 참조 방식 수정
    - ChatBusiness 클래스를 주입받아 메서드를 사용하던 방식에서 직접 내부 로직을 구현하는 방식으로 수정
    - 각 메서드에 로그 적용

--- src/main/java/com/aliens/friendship/backend_chatting_server/websocket/handler/ChatSendWebSocketHandler.java
+++ src/main/java/com/aliens/friendship/backend_chatting_server/websocket/handler/ChatSendWebSocketHandler.java
@@ -1,8 +1,16 @@
 package com.aliens.friendship.backend_chatting_server.websocket.handler;
 
-import com.aliens.friendship.backend_chatting_server.chatting.business.ChatBusiness;
+import com.aliens.friendship.backend_chatting_server.chatting.dto.response.ChatSendResponse;
+import com.aliens.friendship.backend_chatting_server.chatting.service.ChatService;
+import com.aliens.friendship.backend_chatting_server.fcm.service.FcmService;
+import com.aliens.friendship.backend_chatting_server.global.util.jwt.JwtTokenUtil;
+import com.aliens.friendship.backend_chatting_server.websocket.converter.WebsocketConverter;
+import com.aliens.friendship.backend_chatting_server.websocket.dto.request.ChatSendRequest;
+import com.aliens.friendship.backend_chatting_server.websocket.dto.response.ChatSendWebsocketResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -13,19 +21,64 @@ import java.io.IOException;
 import java.util.List;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class ChatSendWebSocketHandler extends TextWebSocketHandler {
     private List<Long> roomIds;
-    private ChatBusiness chatBusiness;
+    private final ChatService chatService;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final ObjectMapper objectMapper;
+    private final FcmService fcmService;
+    private final WebsocketConverter websocketConverter;
+
+    private void sendChat(WebSocketSession session, TextMessage message, List<Long> roomIds) throws FirebaseMessagingException, IOException {
+        // 요청 역직렬화
+        ChatSendRequest chatSendRequest = objectMapper.readValue(message.getPayload(), ChatSendRequest.class);
+
+        // roomId 검증
+        chatService.validateRoomId(chatSendRequest.getRoomId(), roomIds);
+
+        // 채팅 저장
+        ChatSendResponse chatSendResponse = chatService.saveChat(chatSendRequest);
+
+        // 상대방에게 새로운 채팅 FCM 전송
+        fcmService.sendSingleChatToReceiverByToken(chatSendResponse);
+
+        // 채팅 전송 요청에 대한 응답 생성
+        ChatSendWebsocketResponse chatWebSocketResponse = websocketConverter.toChatSendWebsocketResponseWithRequest(chatSendRequest, chatSendResponse);
+
+        // 응답 직렬화
+        String response = objectMapper.writeValueAsString(chatWebSocketResponse);
+
+        // response를 WebSocketSession에 전송
+        session.sendMessage(new TextMessage(response));
+
+        // log
+        log.info("ChatSendWebSocketHandler.sendChat() response: {}", response);
+    }
+
+    private List<Long> afterWebsocketConnectionEstablished(WebSocketSession session) {
+        // header JWT 토큰 가져오기
+        String JWTToken = session.getHandshakeHeaders().get("Authorization").get(0);
+
+        // 토큰 검증
+        jwtTokenUtil.validateToken(JWTToken);
+
+        // log
+        log.info("ChatSendWebSocketHandler.afterWebsocketConnectionEstablished() JWTToken: {}", JWTToken);
+
+        // header token에서 roomId 추출후 반환
+        return jwtTokenUtil.getRoomIdsFromToken(JWTToken);
+    }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException, FirebaseMessagingException {
-        chatBusiness.sendChat(session, message, roomIds);
+        sendChat(session, message, roomIds);
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
-        roomIds = chatBusiness.afterWebsocketConnectionEstablished(session);
+        roomIds = afterWebsocketConnectionEstablished(session);
     }
 
     @Override
